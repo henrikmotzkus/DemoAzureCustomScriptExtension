de10b9d51b234467d4a6752f8b7c2efb105f582a
Henrik Motzkus <hemotzku@microsoft.com>
2021-06-11 12:07:00 +0200

    initial

--- ARM/deploy.ps1
+++ ARM/deploy.ps1
@@ -1,4 +1,18 @@
+###################################################################################################
+#
+# Powershell to deploy the ARM
+#
+
+
 $resouregroupname = "BDF_CustomScriptionExtension1"
+
+
+# Connect to your Azure subscription make sure you're dpeloying to the right subscription!
 Connect-AzAccount 
+
+# Create a resource group 
 New-AzResourceGroup -Name $resouregroupname -Location "westeurope"
+
+
+# Create the deployment
 New-AzResourceGroupDeployment -ResourceGroupName $resouregroupname -TemplateParameterFile "azuredeploy.parameter.json" -TemplateFile "azuredeploy.json"
